dcb96f9ea7322d4426867a62eb60960390ce6526
clcollis <44549711+clcollis@users.noreply.github.com>
2018-11-13 01:11:43 +0000

    pre cloud cannon

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// below here is the accordion
 $( document ).ready(function() {
  $(".dropdown-nav").click(function(){
     $(".dropdown-nav").removeClass("active");
@@ -8,3 +9,43 @@ $( document ).ready(function() {
 
 });
 // above here is the accordion
+
+// below here is smoooth scroll
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+// above here is smoooth scroll
